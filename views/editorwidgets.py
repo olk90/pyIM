bc2d8be292924f353810915446f48529518a2a45
Oliver Köhler <olk.git@posteo.eu>
2021-08-21 23:56:51 +0200

    separated table and editor ui files

--- views/editorwidgets.py
+++ views/editorwidgets.py
@@ -4,6 +4,19 @@ from PySide6.QtWidgets import QWidget
 from views.helpers import load_ui_file
 
 
+class PersonEditorWidget(QWidget):
+
+    def __init__(self, form):
+
+        super().__init__(parent=form)
+        ui_file_name = "ui/personEditor.ui"
+        ui_file = load_ui_file(ui_file_name)
+
+        loader = QUiLoader()
+        self.widget = loader.load(ui_file, form)
+        ui_file.close()
+
+
 class InventoryEditorWidget(QWidget):
 
     def __init__(self, form):
--- views/personwidget.py
+++ views/personwidget.py
@@ -1,5 +1,5 @@
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QWidget, QVBoxLayout
+from PySide6.QtWidgets import QWidget, QHBoxLayout
 
 from views.helpers import load_ui_file
 
@@ -9,12 +9,18 @@ class PersonWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        ui_file_name = "ui/personView.ui"
-        ui_file = load_ui_file(ui_file_name)
-
         loader = QUiLoader()
-        self.widget = loader.load(ui_file)
-        ui_file.close()
 
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.widget)
+        table_ui_name = "ui/personView.ui"
+        table_file = load_ui_file(table_ui_name)
+        self.table = loader.load(table_file)
+        table_file.close()
+
+        editor_ui_name = "ui/personEditor.ui"
+        editor_file = load_ui_file(editor_ui_name)
+        self.editor = loader.load(editor_file)
+        editor_file.close()
+
+        self.layout = QHBoxLayout(self)
+        self.layout.addWidget(self.table, stretch=2)
+        self.layout.addWidget(self.editor, stretch=1)
